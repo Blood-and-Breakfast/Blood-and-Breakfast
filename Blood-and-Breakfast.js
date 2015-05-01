ce993d7e294baa93790f37746ff156d69158d28a
cooper buckingham <cooper.buckingham@gmail.com>
2015-04-30 18:50:29 -0700

    merge, moved code into client and server files

--- Blood-and-Breakfast.js
+++ Blood-and-Breakfast.js
@@ -1,5 +1,5 @@
 
-Markers = new Mongo.Collection('markers');  
+Markers = new Mongo.Collection('markers');
 Stops = new Mongo.Collection("stops");
 Players = new Mongo.Collection("players");
 Buses = new Mongo.Collection("buses");
@@ -24,22 +24,30 @@ if (Meteor.isClient) {
 
   Template.everything.rendered = function(){
     setBodyToWindowSize();
+
   }
 
   Template.everything.created = function() {
     $(window).resize(function() {
       setBodyToWindowSize();
     });
+
+
   };
+
+  Template.header.rendered = function(){
+  }
 
   var setBodyToWindowSize = function(){
     var theBody = $("body");
+
     theBody.height($(window).height());
     theBody.width($(window).width());
   }
 
   Template.registerHelper("playerLoc", function(){
     var loc = Geolocation.currentLocation();
+    if(!loc) return "";
     var locX = loc.coords.latitude;
     var locY = loc.coords.longitude;
     return "" + locX + "," + locY;
@@ -79,51 +87,6 @@ if (Meteor.isClient) {
   });
 }
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-
-    //function is used in _.each statement to get stops while getting routes
-    var getStops = function (tag) {
-      HTTP.get('http://webservices.nextbus.com/service/publicXMLFeed?command=routeConfig&a=sf-muni&r='+tag, function (err, res) {
-              xml2js.parseString(res.content, function (err, parsedJson) {
-                if (err) {
-                  console.log("Error getting stops for Route "+ tag);
-                }
-                var stops = parsedJson.body['route'][0]['stop'];
-                _.each(stops, function (obj) {
-                  Stops.upsert({stopId: obj.$.stopId}, {
-                    tag: obj.$.tag,
-                    title: obj.$.title,
-                    lat: obj.$.lat,
-                    lon: obj.$.lon,
-                    stopId: obj.$.stopId
-                  })
-                })
-              });
-          })
-        };
-    //fetches all routes from NextBus and stores them to MongoDB Routes
-    HTTP.get('http://webservices.nextbus.com/service/publicXMLFeed?command=routeList&a=sf-muni', function (err, res) {
-      //converts XML from NextBus API to JSON
-      if (err) {
-                  console.log("Error getting all Routes");
-                }
-      xml2js.parseString(res.content, function (err, parsedJson){
-        var routes = parsedJson.body.route;
-        _.each(routes, function (obj) {
-          Routes.upsert({tag: obj.$.tag, title: obj.$.title }, {
-            tag: obj.$.tag,
-            title: obj.$.title
-          });
-          // gets all stops for each Route and stores them to MongoDB Stops
-          getStops(obj.$.tag);
-        });
-      });  
-    });
-
-  });
-}
 
 
 
